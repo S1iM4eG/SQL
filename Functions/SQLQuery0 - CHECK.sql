24f2a628fbafae78ee56f6a443e7c358d7cf0244
S1iM <S1iM@HOME-PC>
2026-03-21 22:14:05 +0400

    GetNextLearningDay DONE

--- Functions/SQLQuery0 - CHECK.sql
+++ Functions/SQLQuery0 - CHECK.sql
@@ -1,5 +1,7 @@
 --SQLQuery0 - CHECK.sql
 USE PV_522_Import
 
-Print dbo.GetLastLearningDate(N'PV_522');
-EXEC sp_SelectSchedule;
+--Print dbo.GetLastLearningDate(N'PV_522');
+--EXEC sp_SelectSchedule;
+
+PRINT dbo.GetNextLearningDay(N'PV_522', N'2026-03-19')
